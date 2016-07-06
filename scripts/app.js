8d06a92d1d3003e0d720983e3f4e533ed94e6d42
fjordski <fordheacock@gmail.com>
2016-07-06 11:42:26 -0500

    The Bro's Shopping App
    A simple AngularJS shopping app, tailored to the every day bro.
    
    Style coming soon.

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+var app = angular.module("myShoppingList", []); 
+app.controller("myCtrl", function($scope) {
+    $scope.products = ["Eggs", "Meat", "More Meat", "Beer"];
+		//add items
+		 $scope.addItem = function () {
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.products.indexOf($scope.addMe) == -1) {
+            $scope.products.push($scope.addMe);
+        } else {
+            $scope.errortext = "Bro, you ALREADY HAVE THAT.";
+        }
+    }
+		//remove item at $index position, once
+		$scope.removeItem = function (x){
+			$scope.products.splice(x, 1);
+		}
+		
+		
+});
